fddb09cfbe0f50f9dd0acc8582b884004bab316c
Levi Smith <lasmith@ara.com>
2020-02-10 10:20:49 -0500

    update prettier options

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,4 +11,4 @@ module.exports = {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
   }
-};
+}
