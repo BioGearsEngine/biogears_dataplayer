cd823e2908d28f5c9d3f4540969f32e2cd4df9cf
Levi Smith <lasmith@ara.com>
2020-04-09 14:35:25 -0400

    prototype in progress

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <!--    <img alt="BioGears logo" src="./assets/logo_410.png" />-->
+    <prototype />
+    <chart />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue"
+import Chart from "./components/Chart"
+import Prototype from "./components/Prototype"
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    Chart,
+    Prototype
   }
 }
 </script>
